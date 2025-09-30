f7360e15ae3f923f73096d1f479bd72b781f540d
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-09-30 09:25:53 +0200

    add support for catch2 v2

--- Test/TestSIMPoroElasticity.C
+++ Test/TestSIMPoroElasticity.C
@@ -15,11 +15,7 @@
 #include "SIM2D.h"
 #include "TimeStep.h"
 
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
-
-using Catch::Matchers::WithinAbs;
-using Catch::Matchers::WithinRel;
+#include "Catch2Support.h"
 
 
 TEST_CASE("TestSIMPoroElasticity.Parse")
