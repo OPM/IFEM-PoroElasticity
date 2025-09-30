d5c109c1ea9832a46174abd8bada0eed21cd2bf4
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-09-30 09:25:53 +0200

    add support for catch2 v2

--- Test/TestPoroMaterial.C
+++ Test/TestPoroMaterial.C
@@ -12,12 +12,8 @@
 
 #include "PoroMaterial.h"
 
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include "Catch2Support.h"
 #include <tinyxml2.h>
-
-using Catch::Matchers::WithinAbs;
-using Catch::Matchers::WithinRel;
 
 
 TEST_CASE("TestPoroMaterial.Parse")
